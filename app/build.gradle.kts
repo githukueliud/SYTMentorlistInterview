720517ddf7eb7f3d44a28d2f73a66b380d6f1378
Eliud Githuku <githukueliud@gmail.com>
2023-10-18 23:16:40 +0300

    Final touches

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.sytmentorlistinterview"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.sytmentorlistinterview"
--- app/src/main/java/com/example/sytmentorlistinterview/MainActivity.kt
+++ app/src/main/java/com/example/sytmentorlistinterview/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,6 +23,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -39,22 +41,34 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     //app entry comes here
-                    Column(
-                        verticalArrangement = Arrangement.Center
-                    ){
-                        Text(
-                            text = "Upcoming Trips"
-                        )
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceEvenly
-                        ) {
-                            CardComponent(text = "Dates & Dinner")
-                            CardComponent(text = "Beach & Sand")
-                        }
-                    }
+                    HomeScreen()
                 }
             }
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text(
+            text = "Upcoming Trips",
+            modifier = Modifier.padding(bottom = 50.dp),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            CardComponent(text = "Dates & Dinner")
+            Spacer(modifier = Modifier.height(30.dp))
+            CardComponent(text = "Beach & Sand")
         }
     }
 }
@@ -70,7 +84,7 @@ fun CardComponent(
 
         Card(
             modifier = Modifier
-                .height(250.dp)
+                .height(200.dp)
                 .width(150.dp),
             colors = CardDefaults.cardColors(
                 containerColor = Color(100, 70, 69)
@@ -98,8 +112,17 @@ fun CardComponent(
 
 @Preview(showBackground = true)
 @Composable
-fun CardComponent() {
+fun CardComponentPreview() {
     SYTMentorlistInterviewTheme{
         CardComponent(text = "Sample Text")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    SYTMentorlistInterviewTheme {
+        HomeScreen()
     }
 }
