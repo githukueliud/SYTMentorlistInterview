5f3b5a96691c1145c07ce99bc9e33790d1094797
Eliud Githuku <githukueliud@gmail.com>
2023-11-28 16:56:24 -0500

    Final touches

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    //test
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5:1.5.31")
 }
